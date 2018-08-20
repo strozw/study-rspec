af99e5f3b6b3132b3cce7e632bb5d40408f21923
strozw <sxtxrxoxzxw@gmail.com>
2018-08-20 23:21:04 +0900

    update spec_helper and add sample

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# load application files
+# https://qiita.com/yusabana/items/db44b81bdddf6ed0e9f5
+Dir[File.join(File.dirname(__FILE__), "../lib/**/*.rb")].each { |f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -46,7 +51,6 @@ RSpec.configure do |config|
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
-=begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
@@ -96,5 +100,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-=end
 end
